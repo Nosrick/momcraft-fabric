d2d7f16c78c8776f2cd04276897d125f4814ecb7
nosrick <nosrick@gmail.com>
2020-07-17 17:53:43 +0100

    makes minor import optimisations

--- src/main/java/io/github/nosrick/MoMCraftMod.java
+++ src/main/java/io/github/nosrick/MoMCraftMod.java
@@ -4,10 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.logging.Level;
-
 public class MoMCraftMod implements ModInitializer {
-
 
     public static final Logger logger = LogManager.getLogger();
     public static final String modId = "momcraft";
