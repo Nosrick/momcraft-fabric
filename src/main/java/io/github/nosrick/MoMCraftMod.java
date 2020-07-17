1951de5bbd6f5fa1e14837a23bcafc126be75bc6
nosrick <nosrick@gmail.com>
2020-07-17 17:49:30 +0100

    Initial commit. Includes ModMenu API implementation.

---
+++ src/main/java/io/github/nosrick/MoMCraftMod.java
@@ -0,0 +1,19 @@
+package io.github.nosrick;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.logging.Level;
+
+public class MoMCraftMod implements ModInitializer {
+
+
+    public static final Logger logger = LogManager.getLogger();
+    public static final String modId = "momcraft";
+
+    @Override
+    public void onInitialize() {
+        logger.info("MoMCraft initialised!");
+    }
+}
---
+++ src/main/java/io/github/nosrick/dependency/ModMenuApiImpl.java
@@ -0,0 +1,6 @@
+package io.github.nosrick.dependency;
+
+import io.github.prospector.modmenu.api.ModMenuApi;
+
+public class ModMenuApiImpl implements ModMenuApi {
+}
